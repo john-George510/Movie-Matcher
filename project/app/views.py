9435827cee713a6d330d3a0b59e352263d8d396e
john-George510 <meetjohn510@gmail.com>
2023-05-07 15:40:59 +0530

    actors added to db

--- project/app/views.py
+++ project/app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-import face_recognition,os,cv2,time,requests
+import face_recognition,cv2,requests
 from PIL import Image
 import numpy as np
+from .models import Actor
 
 api_key = "95fd2f005505fb691f27cb9d308ede9f"
 base_url = "https://api.themoviedb.org/3"
@@ -14,35 +15,17 @@ def result(request):
     if request.method == 'POST':
         print(request.POST)
         video_file=str(request.FILES['file'])
-        # actors=actor_detect(video_file)
-        movie=movie_search(['Zendaya Coleman','Timothee chalamet'])
+        actors=actor_detect(video_file)
+        movie=movie_search(actors)
         details=movie_details(movie)
         return render(request,'result.html',{'movie':movie,'release_date':details['release_date'],'Overview':details['overview'],'Rating':details['vote_average'],'genres':details['genres'],'poster':details['poster_url']})
 
 def actor_detect(video_file):
-    start_time=time.time()
-    actors_dir = 'Actors'
-    actors=os.listdir(actors_dir)
-    actor_encodings={}
-    i=0
-    type(actors)
-    for actor in actors[3:43]:
-        i+=1
-        print(i,actor,'encoding',time.time()-start_time)
-        actor_images = os.listdir(os.path.join(actors_dir, actor))
-        actor_encoding = []
-        print(actor_images)
-        for image in actor_images:
-            print(image)
-            actor_image = face_recognition.load_image_file(os.path.join(actors_dir, actor, image))
-            actor_encoding.append(face_recognition.face_encodings(actor_image)[0])
-        actor_encodings[actor] = actor_encoding
-
     video_capture = cv2.VideoCapture(video_file)
     fps = round(video_capture.get(cv2.CAP_PROP_FPS))
     frame_interval = 10
     frame_count = 0
-    print('videocaptured')
+    print('video captured')
     actors_present=[]
     while True:
         ret, frame = video_capture.read()
@@ -53,7 +36,7 @@ def actor_detect(video_file):
             continue
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         face_locations = face_recognition.face_locations(frame_rgb)
-        print("face_locations",face_locations,"\t\tclip time",frame_count/fps)
+        # print("face_locations",face_locations,"\t\tclip time",frame_count/fps)
         for face_location in face_locations:
             top, right, bottom, left = face_location
             face_image = Image.fromarray(frame_rgb[top:bottom, left:right])
@@ -64,23 +47,14 @@ def actor_detect(video_file):
             else:
                 print("No faces detected in the image.")
                 continue
-            i=0
-            for actor in actor_encodings:
-                actor_encoding=actor_encodings[actor]
-                for image in actor_encoding:
-                    results=face_recognition.compare_faces([image],unknown_encoding)
-                    if results[0]:
-                        actors_present.append(actor)
-                        print(actor,"is present")
-                        break
-                else:
-                    continue
-                break
-    print(actors_present)
+            for image in Actor.objects.all():
+                encoding=image.encoding
+                results=face_recognition.compare_faces([encoding],unknown_encoding)
+                if results[0]:
+                    actors_present.append(image.name)
+                    print(image.name,"is present")
+                    break
     return actors_present
-    if len(actors_present):
-        print("actors present in clip are:",actors_present)
-
 def movie_search(actors):
     
     movie_credits = {}
@@ -112,25 +86,17 @@ def movie_search(actors):
 
 def movie_details(movie):
     search_endpoint = "/search/movie"
-    
-    # Define the search query parameters
     query_params = {
         "api_key": api_key,
         "query": str(movie),
         "language": "en-US",
         "page": 1
     }
-
-    # Get movie title input from user
     response = requests.get('https://api.themoviedb.org/3' + search_endpoint, params=query_params)
-
     json_data = response.json()
-
-    # Check if movie was found
     if json_data['total_results'] == 0:
         print('Movie not found.')
     else:
-        # Get first movie details
         movie = json_data['results'][0]
         url = f'https://api.themoviedb.org/3/genre/movie/list?api_key={api_key}'
         response = requests.get(url)
@@ -138,7 +104,6 @@ def movie_details(movie):
             genres = {}
             for genre in response.json()['genres']:
                 genres[genre['id']] = genre['name']
-        # Print movie details
         print('Title:', movie['title'])
         print('Release date:', movie['release_date'])
         print('Overview:', movie['overview'])
@@ -152,3 +117,21 @@ def movie_details(movie):
         movie['poster_url']=poster_url
     return movie
 
+
+
+
+
+
+
+
+# code to add new actors to database
+# actors_dir = 'Actors'
+# actors=os.listdir(actors_dir)
+# for actor in actors:
+#     actor_images = os.listdir(os.path.join(actors_dir, actor))
+#     for image in actor_images:
+#         actor_image = face_recognition.load_image_file(os.path.join(actors_dir, actor, image))
+#         encoding=face_recognition.face_encodings(actor_image)[0]
+#         new_actor = Actor.objects.create(name=actor, encoding=encoding)
+#         new_actor.save()
+        
